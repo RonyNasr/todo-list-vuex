e9358e83de2ce6116ef9bb91f4a54d6f19379926
Rony Nasr <nasr.rony@gmail.com>
2017-11-29 08:30:58 -0800

    added mutations and getters testing

---
+++ test/unit/specs/mutations.spec.js
@@ -0,0 +1,60 @@
+import * as todo from '@/store/modules/todo'
+
+describe('mutations', () => {
+  it('GET_TODO', () => {
+    // mock state
+    const state = {
+      newTodo: ''
+    }
+    todo.default.mutations.GET_TODO(state, 'todo 1')
+    expect(state.newTodo).to.equal('todo 1')
+  })
+  it('ADD_TODO', () => {
+    // mock state
+    const state = {
+      todoList: [],
+      newTodo: 'todo 1'
+    }
+    todo.default.mutations.ADD_TODO(state)
+    expect(state.todoList[0]).to.deep.equal({body: 'todo 1', done: false})
+  })
+})
+
+describe('getters', () => {
+  it('newTodo', () => {
+    // mock state
+    const state = {
+      newTodo: 'todo 1'
+    }
+
+    const result = todo.default.getters.newTodo(state)
+
+    expect(result).to.equal('todo 1')
+  })
+  it('todos', () => {
+    // mock state
+    const state = {
+      todoList: [
+        {body: 'todo1', done: false},
+        {body: 'todo2', done: true}
+      ]
+    }
+
+    const result = todo.default.getters.todos(state)
+
+    expect(result).to.deep.equal([{body: 'todo1', done: false}])
+  })
+  it('completedTodos', () => {
+    // mock state
+    const state = {
+      todoList: [
+        {body: 'todo1', done: false},
+        {body: 'todo2', done: true}
+      ]
+    }
+
+    const result = todo.default.getters.completedTodos(state)
+
+    expect(result).to.deep.equal([{body: 'todo2', done: true}])
+  })
+})
